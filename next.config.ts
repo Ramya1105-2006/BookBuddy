892ac394a02db1006399c62a77b721e3e0573768
aura <mayflower1156@gmail.com>
2025-12-22 04:21:23 +0000

    https://m.media-amazon.com/images/M/MV5BMTQ4MzQzMzM2Nl5BMl5BanBnXkFtZTgw

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
